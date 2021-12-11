ed28f1bfce8f1b27984ba09a46dea029670f0dc3
JinhuaSu <944866518@qq.com>
2021-12-11 04:23:26 +0000

    test it

--- inst/htmlwidgets/mywidget.js
+++ inst/htmlwidgets/mywidget.js
@@ -65,34 +65,38 @@ HTMLWidgets.widget({
         // el.innerHTML = x.message;
         // el.innerHTML = '<div class="spinitron-js-widget" data-action="now-playing-v2" data-station="wzbc">demo</div>'
         // TODO put the necessary attribute here
-        el.setAttribute('class', 'spinitron-js-widget')
-        el.setAttribute('data-action', 'now-playing-v2')
-        el.setAttribute('data-station', 'wzbc')
+        // if (x === 'NetEaseMusic') {
+        el.innerHTML = '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="//music.163.com/outchain/player?type=0&id=4886213342&auto=1&height=430"></iframe>'
 
-        el.setAttribute('data-already-initialized', 'yes');
-        var action = el.getAttribute('data-action');
-        if (['now-playing', 'now-playing-v2', 'upcoming-shows', 'current-playlist'].indexOf(action) < 0) {
-          action = 'now-playing';
-        }
-        var callback = "_spinitron".concat(1).concat((Math.random().toString() + Date.now()).slice(2, -1));
-        var scriptElement;
+        // el.setAttribute('class', 'spinitron-js-widget')
+        // el.setAttribute('data-action', 'now-playing-v2')
+        // el.setAttribute('data-station', 'wzbc')
 
-        window[callback] = function (html) {
-          el.innerHTML = html;
-          scriptElement.parentElement.removeChild(scriptElement);
-          delete window[callback];
-          loadScripts(el);
-        };
+        // el.setAttribute('data-already-initialized', 'yes');
+        // var action = el.getAttribute('data-action');
+        // if (['now-playing', 'now-playing-v2', 'upcoming-shows', 'current-playlist'].indexOf(action) < 0) {
+        //   action = 'now-playing';
+        // }
+        // var callback = "_spinitron".concat(1).concat((Math.random().toString() + Date.now()).slice(2, -1));
+        // var scriptElement;
 
-        var query = ['station', 'num', 'count', 'time', 'nolinks', 'sharing', 'player', 'cover', 'merch', 'meta', 'layout', 'image', 'description'].reduce(function (q, param) {
-          var value = el.getAttribute("data-".concat(param));
-          return value ? "".concat(q, "&").concat(encodeURIComponent(param), "=").concat(encodeURIComponent(value)) : q;
-        }, "callback=".concat(callback));
-        scriptElement = document.createElement('script');
-        scriptElement.src = "//widgets.spinitron.com/widget/".concat(action, "?").concat(query);
-        // action="now-palying"
-        console.log(scriptElement.src)
-        document.getElementsByTagName('head')[0].appendChild(scriptElement);
+        // window[callback] = function (html) {
+        //   el.innerHTML = html;
+        //   scriptElement.parentElement.removeChild(scriptElement);
+        //   delete window[callback];
+        //   loadScripts(el);
+        // };
+
+        // var query = ['station', 'num', 'count', 'time', 'nolinks', 'sharing', 'player', 'cover', 'merch', 'meta', 'layout', 'image', 'description'].reduce(function (q, param) {
+        //   var value = el.getAttribute("data-".concat(param));
+        //   return value ? "".concat(q, "&").concat(encodeURIComponent(param), "=").concat(encodeURIComponent(value)) : q;
+        // }, "callback=".concat(callback));
+        // scriptElement = document.createElement('script');
+        // scriptElement.src = "//widgets.spinitron.com/widget/".concat(action, "?").concat(query);
+        // // action="now-palying"
+        // console.log(scriptElement.src)
+        // document.getElementsByTagName('head')[0].appendChild(scriptElement);
+
 
       },
 
